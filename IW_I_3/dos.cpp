f6560e43b58e22e1df461a46aa8c74b49e30f69c
Railag009 <Solovevkirill26072002@ya.ru>
2023-06-04 23:30:12 +0300

    Добавьте файлы проекта.

---
+++ IW_I_3/dos.cpp
@@ -0,0 +1,86 @@
+﻿#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+	
+	ifstream f;
+	string s;
+	s = "111.txt";	
+	f.open(s);
+	
+	if (!f.is_open())
+	{
+		cout << "eror";
+		return 0;
+	}
+	
+	int n;
+	int i;
+	bool flag = true;
+	int pos = 0;
+	int neg = 0;
+
+	while (!f.eof() && f >> n)
+	{
+		
+		if (n > 0) pos++;
+		else neg++;
+
+	}
+
+	fstream h("333.txt");
+	ofstream g("222.txt");
+	f.close();
+	f.open(s);
+	
+
+
+			
+	if (pos == neg)
+	{
+
+
+		f >> n;
+		g << n << " ";
+		if (n > 0) i = 1;
+		else i = -1;
+
+		while (!f.eof() && f >> n)
+		{
+			
+			if ((n > 0) && (i == 1))
+			{
+				h << n << " ";
+			}
+			else if (n < 0 && i == 1)
+			{
+				g << n << " ";
+				i = -1;
+			}
+			else if (n > 0 && i == -1)
+			{
+				g << n << " ";
+				i = 1;
+			}
+			else
+			{
+				h << n << " ";
+			}
+		}
+
+	}
+	
+	h.close();
+	h.open("333.txt");
+
+	while (!h.eof() && h >> n)
+	{
+
+		g << n << " ";
+
+	}
+	
+}
+
---
+++ uno/uno.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+#include<fstream>
+using namespace std;
+
+struct tree
+{
+    int info;
+    tree* left;
+    tree* right;
+};
+void add_node(tree* root, int data)
+{
+    if (data < root->info)
+    {
+        if (root->left == NULL)
+        {
+            tree* r = new tree;
+            r->info = data;
+            r->left = NULL;
+            r->right = NULL;
+            root->left = r;
+        }
+        else
+            add_node(root->left, data);
+    }
+    else
+    {
+        if (root->right == NULL)
+        {
+            tree* r = new tree;
+            r->info = data;
+            r->left = NULL;
+            r->right = NULL;
+            root->right = r;
+        }
+        else
+            add_node(root->right, data);
+    }
+}
+void print_RNL(tree* root)
+{
+    if (root != NULL)
+    {
+        print_RNL(root->right);
+        cout << root->info << " ";
+        print_RNL(root->left);
+    }
+}
+bool check3(tree* root) {
+    if (root != NULL) {
+        if (root->left != NULL) {
+            if (root->info == root->left->info) {
+                return true;
+            }
+        }
+        if (root->right != NULL) {
+            if (root->info == root->right->info) {
+                return true;
+            }
+        }
+        return check3(root->left) || check3(root->right);
+    }
+    return false;
+}
+int list(tree* t)
+{
+    if (t != NULL)
+    {
+        if (t->left != NULL && t->right != NULL)
+        {
+            return 1 + list(t->left) + list(t->right);
+        }
+        return list(t->left) + list(t->right);
+    }
+    return 0;
+}
+int sum(tree* t)
+{
+    if (t != NULL)
+    {
+        if (t->left != NULL && t->right != NULL)
+        {
+            return t->info + list(t->left) + list(t->right);
+        }
+        return list(t->left) + list(t->right);
+    }
+    return 0;
+}
+void max(tree* t)
+{
+    if (t->right == NULL)t->info *= 2;
+    else
+        max(t->right);
+}
+int main()
+{
+    tree* root = new tree;
+    int x;
+    cin >> x;
+    root->info = x;
+    root->left = NULL;
+    root->right = NULL;
+    for (cin >> x; x != 0; cin >> x)
+        add_node(root, x);
+    if (check3(root) == 1)
+    {
+        int k = list(root);
+        cout << sum(root) / k;
+    }
+    else
+    {
+        max(root);
+        print_RNL(root);
+    }
+
+}
